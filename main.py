8c9639e43ac1320f549a00ec734fdc42ded05c2d
Nikothere <alialekber2876@gmail.com>
2025-11-17 23:59:33 +0400

    save

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
+import os 
+import time
 
 os.system("lsblk")
 
@@ -12,7 +13,7 @@ if swapquest.lower() == "yes":
    os.system(f"mkswap {swappart}")
    os.system(f"swapon {swappart}")
 
-bootformreq = input("Do you want to format the boot partition? (bootloader of other operating systems will be destroyed) (yes/no)")
+bootformreq = input("Do you want to format the boot partition? (bootloader of other operating systems will be destroyed) (yes/no): ")
 
 if bootformreq.lower() == "yes":
    os.system(f"mkfs.fat -F32 {bootpart}")
@@ -25,7 +26,13 @@ os.system(f"mount {rootpart} /mnt")
 os.system(f"mount --mkdir {bootpart} /mnt/boot")
 
 print("Congratulations! The disk setup is complete)")
+time.sleep(3)
+print("Let's move on to the moment of installing packages...")
+time.sleep(3)
+print("You will have installed: efibootmgr, grub, networkmanager, linux, linux-firmware")
 
+print("You can also download your packages")
+print("Do you want to download your packages? (yes/no): ")
 
 
 
