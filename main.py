5bba70ccd3bae80839a3dece918ed239fbedc511
Nikothere <alialekber2876@gmail.com>
2025-11-17 22:00:18 +0400

    Added automatic creation and enabling of the swap section

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ rootpart = input("choose the root partion: ")
 bootpart = input("choose the boot partion: ")
 swapquest = input("Do you want to use swap partition? (yes\no): ")
  
+if swapquest == "yes":
+   swappart = input("chose the swap partition: ")
+   os.system(f"mkswap {swappart}")
+
 
 
 
