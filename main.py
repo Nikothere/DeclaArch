0561ea26c56d72499c4b4d434628fcddf743a732
Nikothere <alialekber2876@gmail.com>
2025-11-17 22:13:06 +0400

    Added a function where the user can decide whether to format the boot partition or not

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import os
 
 os.system("lsblk")
 
-rootpart = input("choose the root partion: ")
-bootpart = input("choose the boot partion: ")
+rootpart = input("choose the root partion: (dev/your_root_partition)")
+bootpart = input("choose the boot partion: (dev/your_boot_partition)")
+
 swapquest = input("Do you want to use swap partition? (yes\no): ")
  
 if swapquest == "yes":
    swappart = input("chose the swap partition: ")
    os.system(f"mkswap {swappart}")
    os.system(f"swapon {swappart}")
+
+bootformreq = input("Do you want to format the boot partition? (bootloader of other operating systems will be destroyed) (yes\no)")
+
+if bootformreq == "yes":
+   os.system(f"mkfs.fat -F32 -n EFI {bootpart}")
+
+
+
 
 
 
