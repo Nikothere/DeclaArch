3a2b450b8a43b88bd4c328ecccbcaaaff1385242
Nikothere <alialekber2876@gmail.com>
2025-11-17 21:32:04 +0400

    Added variables for sections

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ import os
 
 os.system("lsblk")
 
+rootpart = input("choose the root partion: ")
+bootpart = input("choose the boot partion: ")
+swapquest = input("Do you want to use swap partition? (yes\no): ")
+ 
+
+
 
