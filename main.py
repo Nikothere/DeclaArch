75509e8ea1b3c72972277d69f89b3f7c8c166062
Nikothere <alialekber2876@gmail.com>
2025-11-17 22:17:09 +0400

    Fixed a bug in the code with \n

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ os.system("lsblk")
 rootpart = input("choose the root partion: (dev/your_root_partition)")
 bootpart = input("choose the boot partion: (dev/your_boot_partition)")
 
-swapquest = input("Do you want to use swap partition? (yes\no): ")
+swapquest = input("Do you want to use swap partition? (yes/no): ")
  
 if swapquest == "yes":
    swappart = input("chose the swap partition: ")
    os.system(f"mkswap {swappart}")
    os.system(f"swapon {swappart}")
 
-bootformreq = input("Do you want to format the boot partition? (bootloader of other operating systems will be destroyed) (yes\no)")
+bootformreq = input("Do you want to format the boot partition? (bootloader of other operating systems will be destroyed) (yes/no)")
 
 if bootformreq == "yes":
    os.system(f"mkfs.fat -F32 -n EFI {bootpart}")
